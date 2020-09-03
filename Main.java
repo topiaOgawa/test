b42c72b6bf6d4eff14e9f563a6eb555b630765fd
topiaOgawa <ogawa_topia@yahoo.co.jp>
2020-09-03 14:14:59 +0900

    add new file

---
+++ Main.java
@@ -0,0 +1,23 @@
+package code0_1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+      System.out.println("あ");
+      System.out.println("い");
+      System.out.println("う");
+      System.out.println("え");
+      System.out.println("お");
+      System.out.println("31 + 31の計算をします");
+      System.out.println(31 + 31);
+      System.out.println(35 - 10);
+      System.out.println(-5 * 2);
+      System.out.println(6 * 6 * 3.14);
+      System.out.println("こたえは" + 64);
+      int x;
+      x = 6;
+      System.out.println(x * x * 3.14);
+      }
+
+}
